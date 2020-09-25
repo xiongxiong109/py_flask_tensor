fc59c1942f465e12be291340e56da7be4f557327
jqxiong <jqxiong@trip.com>
2020-09-25 15:02:25 +0800

    login

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -3,9 +3,12 @@
 from flask import (
     Blueprint, flash,
     render_template,
-    request
+    request, session
 )
-from werkzeug.security import generate_password_hash
+from werkzeug.security import (
+    generate_password_hash,
+    check_password_hash
+)
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -44,3 +47,37 @@ def register():
         # 如果有表单校验报错，返回对应报错信息
         error is not None and flash(error)
     return render_template('auth/register.html')
+
+
+# 登录页面
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    # 处理post请求
+    if request.method == 'POST':
+        db = get_db()
+        form = request.form
+        unm = form['user_nm']
+        pwd = form['user_pwd']
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?',
+            (unm, )
+        ).fetchone()
+        if user is None:
+            error = 'user not found'
+        # 校验密码
+        else:
+            is_pwd_correct = check_password_hash(user['password'], pwd)
+            if not is_pwd_correct:
+                error = 'password error'
+        if error is None:
+            session.clear()
+            session['user_nm'] = user['username']
+        error is not None and flash(error)
+
+    # get, 渲染登录页面
+    # print(session.get('user_nm'))
+    return render_template(
+        'auth/login.html',
+        user_nm=session.get('user_nm')
+    )
