5f17606ff966b8e75141ec297712a14519032638
jqxiong <jqxiong@trip.com>
2020-10-23 16:14:00 +0800

    update

--- fe/src/pages/blog/index.vue
+++ fe/src/pages/blog/index.vue
@@ -1,3 +1,31 @@
 <template>
-    <p>blog index</p>
+    <div class="blog-wrapper">
+        <p>blog Index</p>
+        <ul v-if="list.length">
+            <li v-for="item in list" :key="item.id">
+                <p>{{item.body}}</p>
+            </li>
+        </ul>
+        <div v-else>
+            <p>暂无文章列表</p>
+            <button @click="$router.push('/create')">发表一篇吧</button>
+        </div>
+    </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            list: []
+        }
+    },
+    async created() {
+        try {
+            const rst = await this.$post('/articles')
+            this.list = rst.data.list || []
+        } catch(err) {
+
+        }
+    }
+}
+</script>
--- flaskr/blog.py
+++ flaskr/blog.py
@@ -2,6 +2,7 @@
 from flask import (
     Blueprint, render_template
 )
+from flaskr.db import get_db
 from flaskr.auth import login_required
 
 # 博客蓝图没有url_prefix，所以指向/
@@ -14,3 +15,19 @@ bp = Blueprint('blog', __name__)
 @login_required
 def index():
     return render_template('blog/index.html')
+
+
+# 查询博客列表
+@bp.route('/articles', methods=['POST'])
+def fetch_article_list():
+    db = get_db()
+    articles = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+
+    return {
+        "list": articles
+    }
+    # return articles or []
